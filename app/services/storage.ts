bb9ae5db621be8f91b61fcd0bd9d8199323dd7b2
balkacer <balcacer.enmanuel@gmail.com>
2023-01-22 16:02:42 -0400

    Storage service created

---
+++ app/services/storage.ts
@@ -0,0 +1,6 @@
+export function setInStorage<T>(key: string, data: T): void {
+
+}
+export function getFromStorage<T>(key: string, ...propsOrIndexes: string[] | number[]): T | null {
+  return null
+}
