08e7091a025e2a7a33bbf7e8e9ca73348bb710b3
balkacer <balcacer.enmanuel@gmail.com>
2023-01-22 22:26:45 -0400

    Update Mocks

--- app/mocks/photos.ts
+++ app/mocks/photos.ts
@@ -2,14 +2,14 @@ import { Photo } from "@prisma/client"
 
 const PHOTOS: Omit<Photo, "createdAt" | "updatedAt">[] = [
   {
-    id: "1",
+    id: "abd5a1e3-f5f3-4750-a3c8-7c5d7616d5bf",
     url: "https://i.ibb.co/Gdhc7cm/user-1.webp",
     detailsUrl: "https://ibb.co/3TJB1BK",
     fileName: "user-1.webp",
     isActive: true
   },
   {
-    id: "2",
+    id: "9615a8fa-cc56-4878-b7e1-0de42da56515",
     url: "https://i.ibb.co/dK2ns3s/user-2.jpg",
     detailsUrl: "https://ibb.co/nkchqVq",
     fileName: "user-2.jpg",
--- app/mocks/users.ts
+++ app/mocks/users.ts
@@ -5,18 +5,18 @@ const USERS: Omit<User, "createdAt" | "updatedAt">[] = [
     name: "Ana",
     lastName: "Cuevas",
     email: "a.cuevas@gmail.com",
-    id: "1",
+    id: "573f373d-efc5-4d28-bb5b-957082a9cc8c",
     password: "1234",
-    photoId: "1",
+    photoId: "9615a8fa-cc56-4878-b7e1-0de42da56515",
     isActive: true,
   },
   {
     name: "Jose",
     lastName: "Perez",
     email: "j.perez@gmail.com",
-    id: "2",
+    id: "db01cac4-8d19-4654-b45c-c93da012d347",
     password: "1234",
-    photoId: "2",
+    photoId: "abd5a1e3-f5f3-4750-a3c8-7c5d7616d5bf",
     isActive: true,
   }
 ]
