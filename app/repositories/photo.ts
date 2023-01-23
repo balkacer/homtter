c8cd0181bf2746795de51c5d42da217ba001a94f
balkacer <balcacer.enmanuel@gmail.com>
2023-01-23 13:36:38 -0400

    Change get photo method to use photo's mock

--- app/repositories/photo.ts
+++ app/repositories/photo.ts
@@ -1,13 +1,26 @@
 import { Photo } from "@prisma/client";
+import PHOTOS from "~/mocks/photos";
 import { Response } from "~/models";
+import asyncExecute from "~/utils/delay";
 
 export async function getPhotoUrlById(id: string): Promise<Response<string>> {
-  return new Promise((resolve) => resolve({
+  const photo = await asyncExecute(() => PHOTOS.find(photo_ => photo_.id === id), 1000);
+
+  if (!photo) {
+    return {
+      message: "This doesn't exist",
+      success: false,
+      messageType: "WARNING",
+      data: null
+    }
+  }
+
+  return {
     message: "All good!",
     success: true,
     messageType: "INFO",
-    data: null
-  }));
+    data: photo?.url
+  }
 }
 
 export async function createPhoto(photo: any): Promise<Response<Photo>> {
