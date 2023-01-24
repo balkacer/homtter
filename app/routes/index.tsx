760ad6f7d7c24f507eec0982aae70dd21dc100de
balkacer <balcacer.enmanuel@gmail.com>
2023-01-24 03:37:02 -0400

    Implement Navbar component on Home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,28 @@
 import { ActionFunction, LoaderFunction } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
 import AuthService from "~/services/auth.server";
-import stylesUrl from "~/styles/index.css";
 import { Form, useLoaderData } from "@remix-run/react";
+import Navbar from "~/components/Navbar";
 
 export default function Index() {
-  const data = useLoaderData();
+  const loggedUser = useLoaderData();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <p>{data?.name} {data?.lastName}</p>
-      <p>{data?.email}</p>
-      <img width={100} height={100} src={data?.profilePicture} />
-
-      <Form method="post">
-        <button>Log Out</button>
-      </Form>
-    </div>
+    <>
+      <Navbar loggedUser={loggedUser} />
+      <main>
+        <h1>Esta es la página de inicio</h1>
+      </main>
+    </>
   );
 }
 
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+export const loader: LoaderFunction = async ({ request }) => {
+  return await AuthService.isAuthenticated(request, {
+    failureRedirect: "/sign-in",
+  });
 };
 
 export const action: ActionFunction = async ({ request }) => {
   await AuthService.logout(request, { redirectTo: "/sign-in" });
-};
-
-export let loader: LoaderFunction = async ({ request }) => {
-  return await AuthService.isAuthenticated(request, {
-    failureRedirect: "/sign-in",
-  });
 };
