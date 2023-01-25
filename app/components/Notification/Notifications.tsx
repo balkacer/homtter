37b88375766ad3a6211efe9913e04c7e83153658
balkacer <balcacer.enmanuel@gmail.com>
2023-01-25 04:46:32 -0400

    Cleaning Notifications component

--- app/components/Notification/Notifications.tsx
+++ app/components/Notification/Notifications.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import Notification, { MessageType, NotificationProps } from "./Notification";
+import Notification, { MessageType } from "./Notification";
 
 export type Notifications = {
   id: string;
@@ -8,16 +7,12 @@ export type Notifications = {
 }
 
 const NotificationCorner = (props: { notifications: Notifications[], onCloseOne: (id: string) => void }) => {
-  const [notifications, setNotifications] = useState<Notifications[]>(props.notifications)
-
-  const closeNotification = (i: number) => {
-
-  }
-
   return (
-    <div style={{ position: "absolute", right: 0, top: 0, padding: 20, width: "100%", maxWidth: 100, display: "flex", flexDirection: "column", gap: 10 }}>
-      {notifications.map(({ content, type, id }, index) => (
-        <Notification key={id} type={type} onClose={() => props.onCloseOne(id)}>
+    <div style={{ position: "absolute", right: 0, top: 0, padding: 20, width: "auto", maxWidth: 400, display: "flex", flexDirection: "column", gap: 10 }}>
+      {props.notifications.map(({ content, type, id }) => (
+        <Notification key={id} type={type} onClose={() => {
+          props.onCloseOne(id);
+        }}>
           {content}
         </Notification>
       ))}
