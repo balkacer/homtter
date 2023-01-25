1ab6ab72a753936f24f3bba415de9788d9f04800
balkacer <balcacer.enmanuel@gmail.com>
2023-01-25 05:06:54 -0400

    Fixed auth issues

--- app/routes/sign-in/index.tsx
+++ app/routes/sign-in/index.tsx
@@ -1,44 +1,33 @@
-import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
-import { v4 as uuid } from 'uuid';
-import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
-import { AuthService, SessionStorageService } from "~/services";
-import NavbarLogo from "~/components/Image/navbarLogo";
 import { useEffect, useState } from "react";
+import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { v4 as uuid } from 'uuid';
+import { AuthService } from "~/services";
+import NavbarLogo from "~/components/Image/navbarLogo";
 import NotificationCorner, { Notifications } from "~/components/Notification/Notifications";
+import sessionStorage from "~/sessions";
 
 export default function SignIn() {
   const [notifications, setNotifications] = useState<Notifications[]>([])
 
   const loaderData = useLoaderData();
-  const actionData = useActionData();
 
   useEffect(() => {
     if (loaderData?.error) {
       const newNotification: Notifications = {
         id: uuid(),
-        type: "danger",
+        type: "warning",
         content: loaderData?.error ? loaderData.error.message : ""
       }
 
       setNotifications(prev => [...prev, newNotification])
     }
-  }, [loaderData])
 
-  useEffect(() => {
-    if (actionData?.error) {
-      const newNotification: Notifications = {
-        id: uuid(),
-        type: "warning",
-        content: actionData?.error ? actionData.error.message : "Something went wrong..."
-      }
-
-      setNotifications(prev => [...prev, newNotification])
-    }
-  }, [actionData])
+    console.log(notifications);
+  }, [loaderData?.error?.message])
 
   const removeNotification = (id: string) => {
-    const tmp = [...notifications];
-    setNotifications(tmp.filter((not) => not.id !== id))
+    setNotifications(prev => prev.filter((not) => not.id !== id))
   }
 
   return (
@@ -79,33 +68,23 @@ export default function SignIn() {
   );
 }
 
-export const loader: LoaderFunction = async ({ request }) => {
-  await AuthService.isAuthenticated(request, {
-    successRedirect: "/"
+export const action: ActionFunction = async ({ request }) => {
+  await AuthService.authenticate("form", request, {
+    successRedirect: "/dashboard",
+    failureRedirect: "/sign-in"
   });
-
-  const session = await SessionStorageService.getSession(
-    request.headers.get("Cookie")
-  );
-
-  const error = session.get("__clientSession");
-  return json<any>({ error });
 };
 
-export const action: ActionFunction = async ({ request }) => {
-  try {
-    await AuthService.authenticate("form", request, {
-      successRedirect: "/",
-      failureRedirect: "/sign-in"
-    });
+export const loader: LoaderFunction = async ({ request }) => {
+  await AuthService.isAuthenticated(request, {
+    successRedirect: "/dashboard"
+  });
 
-    return undefined;
-  } catch (err) {
-    const session = await SessionStorageService.getSession(
-      request.headers.get("Cookie")
-    );
+  const session = await sessionStorage.getSession(request.headers.get("Cookie"))
 
-    const error = session.get("__clientSession");
-    return json<any>({ error });
-  }
+  const error = session.get(AuthService.sessionErrorKey);
+
+  console.log(error);
+
+  return json({ error })
 };
