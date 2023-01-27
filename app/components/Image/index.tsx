53cb31035beadb7f04f032861c0a3ec3c3bbd6c2
balkacer <balcacer.enmanuel@gmail.com>
2023-01-26 23:25:08 -0400

    testing new bulma's classes for get covered image

--- app/components/Image/index.tsx
+++ app/components/Image/index.tsx
@@ -1,10 +1,10 @@
 type ImageSize = "16" | "24" | "32" | "48" | "64" | "96" | "128";
 
-const Image = (props: { source: string | undefined, size: ImageSize, isRounded?: Boolean, }) => {
+const Image = (props: { source: string | undefined, size: ImageSize, isRounded?: Boolean }) => {
   const { source, size, isRounded } = props;
   return (
-    <figure className={`image is-${size}x${size}`}>
-      <img className={isRounded && "is-rounded"} src={source} />
+    <figure className={`image is-square is-fullwidth is-${size}x${size}`}>
+      <img className={`has-ratio${isRounded && " is-rounded"}`} src={source} />
     </figure>
   )
 }
