cde71e24611d1b067ffcfcfb2006d699c814491b
balkacer <balcacer.enmanuel@gmail.com>
2023-01-26 23:24:09 -0400

    Navbar modified

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,48 +1,55 @@
 import { Link, useSubmit } from "@remix-run/react"
 import { User } from "~/models"
-import Image from "../Image";
-import NavbarLogo from "../Image/navbarLogo";
+import Image from "~/components/Image";
+import NavbarLogo from "~/components/Image/navbarLogo";
 
 const Navbar = (props: { loggedUser?: User | null, }) => {
   const { loggedUser } = props;
   const submit = useSubmit();
 
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <NavbarLogo />
         </Link>
-        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
+        <div className="navbar-burger" data-target="homtter-navbar">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
       <div id="navbar" className="navbar-menu">
-        <div className="navbar-start">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
-          <Link to="/estates" className="navbar-item">
-            Estates
-          </Link>
+        <div className="navbar-start" style={{ width: "100%" }}>
+          <div className="navbar-item" style={{ width: "100%" }}>
+            <div className="field has-addons is-expanded">
+              <div className="control is-expanded">
+                <input className="input is-fullwidth" type="text" placeholder="Find a repository" />
+              </div>
+              <div className="control">
+                <a className="button is-primary">
+                  Search
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="navbar-end">
           <div className="navbar-item">
-            {loggedUser ? (
-              <div className="navbar-item has-dropdown">
-                <a className="navbar-item">
-                  <Image source={loggedUser.profilePicture ?? undefined} size="64" isRounded />
-                </a>
-                <div className="navbar-dropdown">
+            {!!loggedUser?.email ? (
+              <div className="navbar-item has-dropdown is-hoverable">
+                <div className="navbar-link is-arrowless" style={{ padding: 0 }}>
+                  <Image source={loggedUser.profilePicture ?? undefined} isRounded size="32" />
+                </div>
+                <div className="navbar-dropdown is-right is-boxed">
                   <Link to="/profile" className="navbar-item">
                     Profile
                   </Link>
-                  <Link to="/estates/publish-new" className="navbar-item">
-                    Publish
+                  <Link to="/dashboard" className="navbar-item">
+                    Dashboard
                   </Link>
-                  <a className="navbar-item" onClick={() => submit(null, { method: "post", action: "/" })}>
+                  <hr className="navbar-divider" />
+                  <a className="navbar-item" onClick={() => submit(null, { method: "post", action: "/dashboard" })}>
                     Log Out
                   </a>
                 </div>
@@ -53,7 +60,7 @@ const Navbar = (props: { loggedUser?: User | null, }) => {
                   <strong>Sign up</strong>
                 </Link>
                 <Link to="/sign-in" className="button is-light">
-                  Log in
+                  Sign In
                 </Link>
               </div>
             )}
